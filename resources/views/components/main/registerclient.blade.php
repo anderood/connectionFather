e9b0aadceefbb7784b6688dfa12c33bc6768ff3e
Anderson <andersonprrsantos@gmail.com>
2024-07-06 19:23:17 -0300

    criado registro de usuarios e de clientes

---
+++ resources/views/components/main/registerclient.blade.php
@@ -0,0 +1,62 @@
+<div>
+    <div>
+
+        <form class="row g-3">
+            <h2 class="">Cadastro de Pacientes</h2>
+            <div class="col-md-6">
+                <label for="name" class="form-label">Primeiro Nome</label>
+                <input type="text" class="form-control" >
+            </div>
+            <div class="col-md-6">
+                <label for="name" class="form-label">Ultimo Nome</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="col-md-6">
+                <label for="name" class="form-label">Data de Nascimento</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="col-md-4">
+                <label for="inputState" class="form-label">Sexo</label>
+                <select id="inputState" class="form-select">
+                    <option selected>Selecione</option>
+                    <option>Masculino</option>
+                    <option>Feminino</option>
+                </select>
+            </div>
+            <div class="col-md-6">
+                <label for="inputEmail4" class="form-label">Email</label>
+                <input type="email" class="form-control" >
+            </div>
+            <div class="col-md-6">
+                <label for="inputPassword4" class="form-label">Telefone</label>
+                <input type="tel" class="form-control" >
+            </div>
+            <h2>Endereço</h2>
+            <div class="col-md-2">
+                <label for="inputZip" class="form-label">CEP</label>
+                <input type="text" class="form-control">
+            </div>
+            <div class="col-md-2">
+                <label for="inputZip" class="form-label">Rua</label>
+                <input type="text" class="form-control" >
+            </div>
+            <div class="col-md-2">
+                <label for="inputZip" class="form-label">Numero</label>
+                <input type="text" class="form-control" >
+            </div><div class="col-md-2">
+                <label for="inputZip" class="form-label">Complemento</label>
+                <input type="text" class="form-control" >
+            </div><div class="col-md-2">
+                <label for="inputZip" class="form-label">Cidade</label>
+                <input type="text" class="form-control" >
+            </div><div class="col-md-2">
+                <label for="inputZip" class="form-label">Estado</label>
+                <input type="text" class="form-control" >
+            </div>
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary">Confirmar</button>
+            </div>
+        </form>
+    </div>
+
+</div>
---
+++ resources/views/components/main/registeruser.blade.php
@@ -0,0 +1,28 @@
+<div>
+    <h1>Cadastro de Usuario</h1>
+    <form class="row g-3">
+        <div class="col-md-6">
+            <label for="name" class="form-label">Nome Completo</label>
+            <input type="text" class="form-control" id="inputEmail4">
+        </div>
+        <div class="col-md-6">
+            <label for="inputEmail4" class="form-label">Email</label>
+            <input type="email" class="form-control" id="inputEmail4">
+        </div>
+        <div class="col-md-6">
+            <label for="inputPassword4" class="form-label">Telefone</label>
+            <input type="tel" class="form-control" id="inputPassword4">
+        </div>
+        <div class="col-md-6">
+            <label for="inputPassword4" class="form-label">Senha</label>
+            <input type="password" class="form-control" id="inputPassword4">
+        </div>
+        <div class="col-md-6">
+            <label for="inputPassword4" class="form-label">Confirmação da Senha</label>
+            <input type="password" class="form-control" id="inputPassword4">
+        </div>
+        <div class="col-12">
+            <button type="submit" class="btn btn-primary">Confirmar</button>
+        </div>
+    </form>
+</div>
