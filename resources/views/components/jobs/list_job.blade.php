cd5fa93b79c17d44dbfb5ea601a914019e209cb3
Anderson <andersonprrsantos@gmail.com>
2024-07-14 00:51:42 -0300

    correção do formato da data de agendamento

--- resources/views/components/jobs/list_job.blade.php
+++ resources/views/components/jobs/list_job.blade.php
@@ -1,3 +1,4 @@
+@php use Carbon\Carbon; @endphp
 <div class="container">
     <div class="container-sm flex-column">
         <h2>Agendamentos</h2>
@@ -10,7 +11,7 @@
             <th scope="col">Titulo</th>
             <th scope="col">Paciente</th>
             <th scope="col">Colaborador</th>
-            <th scope="col">Data de Agendamento</th>
+            <th scope="col">Agendamento</th>
             <th scope="col">Status</th>
             <th scope="col">Ação</th>
         </tr>
@@ -21,14 +22,16 @@
                 <td>{{$job->title}}</td>
                 <td>{{$job->patient->getFullName()}}</td>
                 <td>{{ $job->user->name }}</td>
-                <td>{{ $job->date_scheduling }}</td>
+                <td>{{ Carbon::parse($job->date_scheduling)->format('d/m/Y') }}</td>
                 <td>{{ $job->status[0]['title'] }}</td>
                 <td>
                     <a href="{{ route('jobs.edit_job', [$job->id]) }}" class="btn btn-primary">Editar</a>
                     <form action="{{ route('jobs.destroy', [$job->id]) }}" method="POST" class="d-inline">
                         @csrf
                         @method('DELETE')
-                        <button type="submit" class="btn btn-danger" onclick="return confirm('Tem certeza que deseja excluir este usuário?')">Excluir</button>
+                        <button type="submit" class="btn btn-danger"
+                                onclick="return confirm('Tem certeza que deseja excluir este usuário?')">Excluir
+                        </button>
                     </form>
                 </td>
             </tr>
