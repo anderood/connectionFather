a7bbadef16deec51b38cca6365156fbada561192
Anderson <andersonprrsantos@gmail.com>
2024-07-16 03:05:21 -0300

    ajustado form para criação do novo colaborador

--- resources/views/components/users/create_user.blade.php
+++ resources/views/components/users/create_user.blade.php
@@ -1,5 +1,5 @@
 <div class="container">
-    <h2>Novo Colaborador</h2>
+    <h2 class="text-center mb-4">Novo Colaborador</h2>
     @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
@@ -12,27 +12,23 @@
     <form class="row g-3" action="{{ route('users.store') }}" method="post">
         @csrf
         @method('post')
-        <div class="col-md-6">
+        <div class="col-md-12">
             <label for="name" class="form-label">Nome Completo</label>
-            <input type="text" class="form-control" name="name">
+            <input type="text" class="form-control" name="name" required>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-12">
             <label for="email" class="form-label">Email</label>
-            <input type="email" class="form-control" name="email">
+            <input type="email" class="form-control" name="email" required>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-12">
             <label for="phone" class="form-label">Telefone</label>
-            <input type="tel" class="form-control" name="phone">
+            <input type="tel" class="form-control" name="phone" required>
         </div>
-        <div class="col-md-6">
+        <div class="col-md-12">
             <label for="password" class="form-label">Senha</label>
-            <input type="password" class="form-control" name="password">
+            <input type="password" class="form-control" name="password" required>
         </div>
-{{--        <div class="col-md-6">--}}
-{{--            <label for="inputPassword4" class="form-label">Confirmação da Senha</label>--}}
-{{--            <input type="password" class="form-control" id="inputPassword4">--}}
-{{--        </div>--}}
-        <div class="col-12">
+        <div class="col-12 text-center">
             <button type="submit" class="btn btn-primary">Criar</button>
         </div>
     </form>
